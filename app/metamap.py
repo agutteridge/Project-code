d500f6e988b7bd1a233df349186f10c683d60d66
Alice <alicegutteridge@gmail.com>
2015-07-31 00:11:55 +0100

    python java MetaMap almost works but currently requires user input

---
+++ app/metamap.py
@@ -0,0 +1,81 @@
+# The Java .jar that calls the Medical Text Indexer is invoked from this file
+
+import json, subprocess, datetime, os, time
+import config, citations
+
+# Creates a batch ID for .txt files to be used as input for MetaMap
+def create_batch_id():
+    # removes last 4 digits so ms is 2 s.f.
+    return datetime.datetime.now().strftime("%Y%m%d_%H%M%S%f")[:-4] 
+
+def run(results):
+    batch_id = create_batch_id()
+    filename = batch_id + '.txt'
+    batch = open(filename, 'a+') # default: unbuffered
+    for i in range(0, len(results)):
+        ASCII_title = results[i]['MedlineCitation']['Article']['ArticleTitle'].encode('ascii', 
+            errors='ignore').decode('UTF-8')
+        ASCII_abstract = results[i]['MedlineCitation']['Article']['Abstract']['AbstractText'][0].encode('ascii', 
+            errors='ignore').decode('UTF-8')
+        batch.write('UI  - ' + batch_id + str(i) + '\n' + 
+            'TI  - ' + ASCII_title + '\n' +
+            'AB  - ' + ASCII_abstract + 
+            '\n\n')
+
+    print('done!')
+    done = True
+
+    mp = config.metamap_path
+
+    # shell args for running MetaMapCaller.jar
+    popen_args = ['java',
+                  '-cp',
+                  mp + '/classes:' +
+                  mp + '/lib/skrAPI.jar:' +
+                  mp + '/lib/commons-logging-1.1.1.jar:' +
+                  mp + '/lib/httpclient-cache-4.1.1.jar:' +
+                  mp + '/lib/httpcore-nio-4.1.jar:' +
+                  mp + '/lib/httpclient-4.1.1.jar:' +
+                  mp + '/lib/httpcore-4.1.jar:' +
+                  mp + '/lib/httpmime-4.1.1.jar:' +
+                  '.:' +
+                  config.json_simple_path,
+                  'MetaMapCaller',
+                  '../../' + filename, 
+                  config.email]
+
+    if done:
+        p = subprocess.Popen(popen_args, cwd='./java/bin/', stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        # time.sleep(1)
+        # print(config.email)
+        # # time.sleep(10)
+        # print('oh hey password')
+        # print('ohheypasswrod')
+        # p.stdin.write(bytes(config.email + '\n', 'UTF-8'))
+        # p.communicate(input=bytes(config.pwd + '\n', 'UTF-8'))
+        # p.communicate(input=bytes(config.email + '\n', 'UTF-8'))
+        # p.stdin.write(bytes(config.email + '\n', 'UTF-8'))
+
+        # p.stdin.write('\t' + config.email + '\n')
+        # p.communicate('\t' + config.pwd + '\n')
+        lines_iterator = iter(p.stdout.readline, b"") #sentinel?!
+        
+        for line in lines_iterator:
+            print(line)        
+
+        # for line in enumerate(lines_iterator):
+        #     print('line number is: ' + str(line[0]))
+        #     if line[0] is 0:
+        #         p.stdin.write(config.email)
+        #         p.communicate
+        #     elif line[0] is 1:
+        #         p.stdin.write(config.pwd)
+        #     else:
+        #         # MetaMap data
+        #         print(line[1])
+
+# testing..
+if __name__ == "__main__":
+    with open(os.path.join('../tests/resources', 'eFetch_sample.json'), 
+            'r') as datafile:
+        run(json.load(datafile))
