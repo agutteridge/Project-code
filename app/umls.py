fe15b19c41e63f5ba2526b51098112bd650d7413
Alice <alicegutteridge@gmail.com>
2015-08-12 19:47:39 +0100

    one name for CUI chosen according to MRRANK of sources

--- app/umls.py
+++ app/umls.py
@@ -8,6 +8,7 @@ def run_with_data(paper_terms):
         concepts = p['concepts']
         for c in concepts:
             cui_list.append(c[1])
+
     # Connect to the database
     connection = pymysql.connect(host='localhost',
                                  user=config.db_un,
@@ -18,55 +19,21 @@ def run_with_data(paper_terms):
 
     try:
         with connection.cursor() as cursor:
-            sql = """SELECT DISTINCT COALESCE(
-                        (SELECT C.`STR` 
-                         FROM `MRCONSO` AS C
-                         WHERE C.`CUI`=M.`CUI`
-                         AND C.`SAB`=%s
-                         AND C.`TTY`=%s 
-                         LIMIT 1),
-                        
-                        (SELECT C.`STR` 
-                         FROM `MRCONSO` AS C
-                         WHERE C.`CUI`=M.`CUI` 
-                         AND C.`SAB`=%s 
-                         AND C.`TTY`=%s 
-                         LIMIT 1),
 
-                        (SELECT C.`STR` 
-                         FROM `MRCONSO` AS C
-                         WHERE C.`CUI`=M.`CUI` 
-                         LIMIT 1)) `Concept name`, M.`CUI`
-                    FROM `MRCONSO` AS M
-                    WHERE M.`CUI` IN %s
-                    AND M.`ISPREF`=%s
-                    AND M.`STT`=%s"""
-            print(sql)
-            cursor.execute(sql, ('MSH', 'MH',
-                                 'MTH', 'PN',
-                                 cui_list, 'Y', 'PF'))
+            sql = """SELECT C1.`AUI`, C1.`CUI`, C1.`STR`
+                     FROM (SELECT C2.`CUI`, MAX(R2.`RANK`) AS MAXRANK
+                           FROM `MRCONSO` AS C2 
+                           INNER JOIN `MRRANK` AS R2 ON (C2.`SAB`=R2.`SAB` AND C2.`TTY`=R2.`TTY`)
+                           WHERE C2.`ISPREF`=%s
+                           AND C2.`STT`=%s
+                           AND C2.`CUI` IN %s
+                           GROUP BY C2.`CUI`) AS T
+                     INNER JOIN `MRRANK` AS R1 ON R1.`RANK`=T.`MAXRANK`
+                     INNER JOIN `MRCONSO` C1 ON (C1.`SAB`=R1.`SAB` AND C1.`TTY`=R1.`TTY`)
+                     WHERE C1.`CUI` = T.`CUI`"""
+
+            cursor.execute(sql, ('Y', 'PF', cui_list))
             result = cursor.fetchall()
-            print(result) # turn into list extract best STR based on SAB and TTY.. then get PAR? or get PAR based on CUI?
-    finally:
-        connection.close()  
-
-def run():
-    # Connect to the database
-    connection = pymysql.connect(host='localhost',
-                                 user=config.db_un,
-                                 password=config.db_pwd,
-                                 db='umls',
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
-
-    example_id = 'C0027051'
-
-    try:
-        with connection.cursor() as cursor:
-            # Read a single record
-            sql = """SELECT C.`STR` FROM `MRCONSO` AS C WHERE `ISPREF`=%s AND `STT`=%s AND `CUI`=%s LIMIT 1"""
-            cursor.execute(sql, ('Y', 'PF', example_id))
-            result = cursor.fetchone()
             print(result)
     finally:
         connection.close()
