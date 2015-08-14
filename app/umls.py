f18a49cde162777578d1106293bf4f1666f0a406
Alice <alicegutteridge@gmail.com>
2015-08-14 14:37:52 +0100

    SQL statement written to get first parents and semantic types of child

--- app/umls.py
+++ app/umls.py
@@ -22,37 +22,28 @@ def run_with_data(paper_terms):
     try:
         with connection.cursor() as cursor:
 
-            var = 'C0008659'
+            sql = """SELECT DISTINCT T2.`CHILD_CUI`, T2.`S_TYPE`, T2.`PARENT1_CUI`, C2.`STR` AS PARENT1_STR
+                      FROM `MRCONSO` AS C2
+                      INNER JOIN (SELECT DISTINCT T1.`CHILD_CUI`, T1.`PARENT1_CUI`, MAX(R1.`RANK`) AS MAXRANK, T1.`S_TYPE`
+                                  FROM `MRCONSO` AS C1
+                                  INNER JOIN `MRRANK` AS R1 ON (C1.`SAB` = R1.`SAB` AND C1.`TTY` = R1.`TTY`)
+                                  INNER JOIN (SELECT DISTINCT M1.`CUI1` AS CHILD_CUI, M1.`CUI2` AS PARENT1_CUI, S.`STY` AS S_TYPE
+                                              FROM `MRREL` AS M1
+                                              INNER JOIN `MRSTY` AS S ON S.`CUI` = M1.`CUI1`
+                                              WHERE M1.`CUI1` IN %s
+                                              AND (M1.`RELA`=%s
+                                                   OR M1.`REL`=%s
+                                                   OR M1.`REL`=%s)) AS T1 ON T1.`PARENT1_CUI` = C1.`CUI` 
+                                  WHERE C1.`STT`=%s
+                                  AND C1.`ISPREF`=%s
+                                  GROUP BY C1.`CUI`) AS T2 ON T2.`PARENT1_CUI` = C2.`CUI`
+                      INNER JOIN `MRRANK` AS R3 ON (R3.`RANK` = T2.`MAXRANK` AND C2.`SAB` = R3.`SAB` AND C2.`TTY` = R3.`TTY`)
+                      WHERE C2.`STT`=%s
+                      AND C2.`ISPREF`=%s"""
 
-            sql1 = """SELECT C1.`CUI`, MRREL.`CUI2`
-                        FROM `MRCONSO` AS C1
-                        INNER JOIN `MRREL` ON MRREL.`CUI1` = C1.`CUI`
-                        INNER JOIN `MRCONSO` AS C2 ON C2.`CUI` = MRREL.`CUI2`
-                        WHERE (MRREL.`RELA`=%s
-                        OR MRREL.`REL`=%s
-                        OR MRREL.`REL`=%s)
-                        AND C1.`CUI` IN %s"""
+            cursor.execute(sql, (cui_list, 'inverse_isa', 'PAR', 'RB', 'PF', 'Y', 'PF', 'Y'))
+            results = cursor.fetchall() #parent IDs
 
-            cursor.execute(sql1, ('inverse_isa', 'PAR', 'RB', cui_list))
-            result = cursor.fetchall()
-
-            use stored procedure instead recursively call
-            either a set number of times, or until a certain stopping point?
-            sql = """SELECT C1.`CUI`, C1.`STR`, 
-                     FROM (SELECT C2.`CUI`, MAX(R2.`RANK`) AS MAXRANK
-                           FROM `MRCONSO` AS C2 
-                           INNER JOIN `MRRANK` AS R2 ON (C2.`SAB`=R2.`SAB` AND C2.`TTY`=R2.`TTY`)
-                           WHERE C2.`ISPREF`=%s
-                           AND C2.`STT`=%s
-                           AND C2.`CUI` IN %s
-                           GROUP BY C2.`CUI`) AS T
-                     INNER JOIN `MRRANK` AS R1 ON R1.`RANK`=T.`MAXRANK`
-                     INNER JOIN `MRCONSO` C1 ON (C1.`SAB`=R1.`SAB` AND C1.`TTY`=R1.`TTY`)
-                     INNER JOIN `MRREL` M1 ON (M1.CUI1 = )
-                     WHERE C1.`CUI` = T.`CUI`"""
-
-            # cursor.execute(sql, ('Y', 'PF', cui_list))
-            result = cursor.fetchall()
     finally:
         connection.close()
 
@@ -62,4 +53,4 @@ def run_with_data(paper_terms):
     #         for c in concepts:
     #             if r['CUI'] == c[1]:
     #                 r.setdefault('PMID', []).append(p['PMID'])
-    return result
+    return results
